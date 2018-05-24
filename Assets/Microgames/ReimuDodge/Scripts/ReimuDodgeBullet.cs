70b710e377880b1cf41c81dd8b3169f7f307471d
Barleytree <barleywald@gmail.com>
2018-05-25 06:44:29 +0900

    Resurrecting old branch

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReimuDodgeBullet : MonoBehaviour
+{
+
+    [Header("How fast the bullet goes")]
+    [SerializeField]
+    private float speed;
+
+    [Header("Firing delay in seconds")]
+    [SerializeField]
+    private float delay;
+
+    // Stores the direction of travel for the bullet
+    private Vector2 trajectory;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Wait before targetting the player
+        this.StartCoroutine(Wait(this.delay));
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Only start moving after the trajectory has been set
+        if (this.trajectory != null)
+        {
+            // Move the bullet a certain distance based on trajectory speed and time
+            Vector2 newPosition = (Vector2)this.transform.position + (this.trajectory * this.speed * Time.deltaTime);
+            this.transform.position = newPosition;
+        }
+    }
+
+    void SetTrajectory()
+    {
+        // Find the player object in the scene and calculate a trajectory towards them
+        GameObject player = GameObject.Find("Player");
+        this.trajectory = (player.transform.position - this.transform.position).normalized;
+    }
+    
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        // Acquire target
+        this.SetTrajectory();
+    }
+    
+}
---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgePlayer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ReimuDodgePlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip deathSound;
+
+    private bool alive = true;
+
+    // This will happen when the player's hitbox collides with a bullet
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Only kill Reimu if she's still alive
+        if (this.alive)
+        {
+            this.DoDeath();
+
+            // Now tell the MicrogameController in the scene that the game is over
+            // and we've lost forever
+            MicrogameController.instance.setVictory(victory: false, final: true);
+        }
+    }
+
+    void DoDeath()
+    {
+        // Kill Reimu
+        this.alive = false;
+
+        // Get a reference to the player's sprite
+        // This is a "slow" operation but we will only need to do it once
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        // Make the sprite disappear by disabling the the GameObject it's attached to
+        // This will also stop the animator because they share a GameObject
+        spriteRenderer.gameObject.SetActive(false);
+
+        // Finally, disable the FollowCursor script to stop the object from moving
+        FollowCursor followCursor = GetComponent<FollowCursor>();
+        followCursor.enabled = false;
+
+        // Play the death sound effect
+        // At a custom volume
+        MicrogameController.instance.playSFX(this.deathSound, volume: 0.5F);
+
+        // Now get a reference to the death exposion and start it
+        ParticleSystem particleSystem = GetComponentInChildren<ParticleSystem>();
+        particleSystem.Play();
+    }
+
+}
